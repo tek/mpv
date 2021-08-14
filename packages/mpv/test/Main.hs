bc57be0a77fddd7c1c8c377ba4c9ddb6a6c51bfc
Torsten Schmits <torstenschmits@gmail.com>
2021-08-14 22:45:03 +0200

    do not run mpv process tests when no X is available

--- packages/mpv/test/Main.hs
+++ packages/mpv/test/Main.hs
@@ -1,15 +1,34 @@
 module Main where
 
+import Mpv.Test.MessageSplitTest (test_messageSplit)
 import Mpv.Test.MpvTest (test_mpv)
 import Mpv.Test.ServerTest (test_server)
-import Test.Tasty (TestTree, defaultMain, testGroup)
 import Polysemy.Test (unitTest)
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.ExpectedFailure (wrapTest)
+import Test.Tasty.Providers (testPassed)
+
+ignoreIfNoX :: TestTree -> TestTree
+ignoreIfNoX =
+  wrapTest \ test ->
+    maybe skip (const test) =<< lookupEnv "XDG_RUNTIME_DIR"
+  where
+    skip =
+      pure (testPassed "mpv test cannot be run without $XDG_RUNTIME_DIR")
+
+mpvTests :: TestTree
+mpvTests =
+  ignoreIfNoX $
+  testGroup "mpv process" [
+    unitTest "basic functionality" test_mpv,
+    unitTest "client/server architecture" test_server
+  ]
 
 tests :: TestTree
 tests =
   testGroup "all" [
-    unitTest "basic functionality" test_mpv,
-    unitTest "client/server architecture" test_server
+    mpvTests,
+    unitTest "split socket messages on newlines" test_messageSplit
   ]
 
 main :: IO ()
--- packages/mpv/test/Mpv/Test/MpvTest.hs
+++ packages/mpv/test/Mpv/Test/MpvTest.hs
@@ -3,9 +3,7 @@ module Mpv.Test.MpvTest where
 import qualified Data.List.NonEmpty as NonEmpty
 import Path (File, Rel, relfile)
 import qualified Polysemy.Conc as Race
-import qualified Polysemy.Conc as Conc
-import Polysemy.Conc (interpretRace, withAsync_)
-import qualified Polysemy.Log as Log
+import Polysemy.Conc (interpretRace)
 import Polysemy.Log.Colog (interpretLogStdoutConc)
 import qualified Polysemy.Test as Test
 import Polysemy.Test (UnitTest, assertClose, assertEq, runTestAuto)
@@ -17,7 +15,6 @@ import qualified Mpv.Data.SeekFlags as SeekFlags
 import Mpv.Data.SeekFlags (SeekFlags (SeekFlags), SeekReference (Absolute), SeekRestart (Exact))
 import Mpv.Data.Track (Track (Track), TrackList (TrackList), TrackType (Audio, Sub, Video))
 import qualified Mpv.Effect.Mpv as Mpv
-import qualified Mpv.Interpreter.Mpv as Mpv
 import Mpv.Interpreter.Mpv (interpretMpvNative, withMpv)
 import qualified Mpv.Mpv as Mpv
 import Mpv.Mpv (addAudioDelay, adjustVolumeBy, setDefaultOptions, togglePlaybackState)
@@ -39,19 +36,18 @@ test_mpv =
     Race.timeout_ () (Seconds 4) do
       withMpv do
         resumeHoistError show do
-          withAsync_ (subscribeLoop (Log.debug . show)) do
-            setDefaultOptions
-            Mpv.command (Command.Load vid)
-            assertEq 3.6 =<< Mpv.prop Property.Duration
-            assertEq 0 =<< Mpv.prop Property.SubFps
-            Mpv.setProp Property.SubFps 100
-            Mpv.setProp Property.SubDelay 1
-            assertEq 100 =<< Mpv.prop Property.SubFps
-            assertEq trackList . NonEmpty.sort . coerce =<< Mpv.prop Property.TrackList
-            void Mpv.info
-            void togglePlaybackState
-            Mpv.setProp Property.Volume 100
-            assertClose 95 =<< adjustVolumeBy (-5)
-            void (addAudioDelay 5)
-            Mpv.command (Command.Seek 50 (SeekFlags Absolute SeekFlags.Percent Exact))
-            void $ Mpv.command Command.Stop
+          setDefaultOptions
+          Mpv.command (Command.Load vid)
+          assertEq 3.6 =<< Mpv.prop Property.Duration
+          assertEq 0 =<< Mpv.prop Property.SubFps
+          Mpv.setProp Property.SubFps 100
+          Mpv.setProp Property.SubDelay 1
+          assertEq 100 =<< Mpv.prop Property.SubFps
+          assertEq trackList . NonEmpty.sort . coerce =<< Mpv.prop Property.TrackList
+          void Mpv.info
+          void togglePlaybackState
+          Mpv.setProp Property.Volume 100
+          assertClose 95 =<< adjustVolumeBy (-5)
+          void (addAudioDelay 5)
+          Mpv.command (Command.Seek 50 (SeekFlags Absolute SeekFlags.Percent Exact))
+          void $ Mpv.command Command.Stop
